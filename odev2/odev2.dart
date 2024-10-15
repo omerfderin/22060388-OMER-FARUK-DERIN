31bdde210e3a87b2f9ce69cba7b5b4ef81fe4211
Ömer Faruk Derin <114663492+omerfderin@users.noreply.github.com>
2024-10-15 22:20:32 +0300

    Add files via upload

---
+++ odev2/odev2.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Buton());
+}
+
+class Buton extends StatefulWidget {
+  const Buton({super.key});
+
+  State <Buton> createState() => Renk();
+}
+
+class Renk extends State<Buton> {
+  int index = -1;
+  List<Color> renkler = [Colors.yellow, Colors.indigo];
+
+  Widget build(BuildContext context) {
+    Color renk = index == -1 ? Colors.white : renkler[index];
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: renk,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    index = (index + 1) % 2;
+                  });
+                },
+                child: Text("RENK DEGİS"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
